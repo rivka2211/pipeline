e0ac5b00b90fc728a782efdf29226288c7efa339
rivka2211 <hitmachut@students.local>
2025-07-18 09:40:11 +0300

    first-commit

---
+++ main.groovy
@@ -0,0 +1,26 @@
+pipeline{
+       agent { label 'verisoft-2' }
+        options {
+            skipStagesAfterUnstable()
+            disableConcurrentBuild()
+        }
+        environment {
+            PROJECT_NAME = 'pipeline-R-E'
+        }
+        parameters {
+            choice(choices: ["cake", "bread", "milk"], name: "Food")
+            string(defaultValue:"Rivki",name:"firstName",description:"my first name")
+            string(defaultValue: "Elfenbein", name: "lastName", description: "my last name")
+        }
+    stages {
+        stage("print parameters"){
+            steps{
+                echo "parameters ${params.Food}"
+                echo "parameters ${params}"
+                echo "environment variable ${env.PROJECT_NAME}"
+                echo "${env.BUILD_NAME}"
+            }
+        }
+    }  
+
+}
